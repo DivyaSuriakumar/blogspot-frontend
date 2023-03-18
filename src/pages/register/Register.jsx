2cac298aca59fea3ad820a169cb97958b9c681a3
Divya Suriakumar <20cs050@kpriet.ac.in>
2023-03-18 19:15:47 +0530

    unchange link

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -13,12 +13,12 @@ export default function Register() {
     e.preventDefault();
     setError(false);
     try {
-      const res = await axios.post("https://blogspot-api.onrender.com/api/auth/register", {
+      const res = await axios.post("/auth/register", {
         username,
         email,
         password,
       });
-      res.data && window.location.replace("https://blogspot-api.onrender.com/api/login");
+      res.data && window.location.replace("/login");
     } catch (err) {
       setError(true);
     }
